36e042b9e82a7f60c95926fbc860ffe465fbdc2a
Lydia Deterding <deterding.lydia@gmail.com>
2021-09-08 21:55:17 -0500

    Fifth Commit

---
+++ send_to_mongo.py
@@ -0,0 +1,26 @@
+from typing import Collection
+import pymongo
+from pymongo import collection
+from mongo_atlas import password
+import json
+
+
+conn = f'mongodb+srv://aarvinrathod:{password}@cluster0.cw8ac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(conn)
+
+db = client.coviddb
+Collection = db.admindata
+Collection.drop()
+
+with open("administration.json") as file:
+    file_data = json.load(file)
+
+Collection.insert_one(file_data)
+
+Collection = db.finaldata
+Collection.drop()
+
+with open("final_data.json") as file:
+    file_data = json.load(file)
+
+Collection.insert_one(file_data)
